84a26623e9aa7c99570a8910b9b6519955feb377
ehsansaleem1 <149003325+ehsansaleem1@users.noreply.github.com>
2023-12-07 23:26:51 +0530

    Update devdesc.jsx

--- src/pages/devdesc.jsx
+++ src/pages/devdesc.jsx
@@ -17,7 +17,7 @@ export default function DevDescPage() {
   useEffect(() => {
     setTimeout(() => {
       async function getDetails() {
-        const resp = await fetch(`https://api.rawg.io/api/developers/${newquery2}?key=7468c0691a294985aee336bff6bfef59`)
+        const resp = await fetch(`https://api.rawg.io/api/developers/${newquery2}?key=API_KEY`)
 
         const jsonresp = await resp.json()
 

c66bd3f0146cbb105a073e3739bd7e35a237fb41
Ehsan Saleem <149003325+ehsansaleem1@users.noreply.github.com>
2024-03-19 10:07:03 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  const env = loadEnv(mode, process.cwd(), '');
+  define: {
+      'process.env.API_KEY': JSON.stringify(env.API_KEY)
+    },
   plugins: [react()],
   server: {
     host: '0.0.0.0',
